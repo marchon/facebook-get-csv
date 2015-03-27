ce855d55ff8ea118975727c427dea8d424a31f99
Alexandr Baklanov <alexandr.baklanov@gmail.com>
2015-03-27 15:58:39 +0400

    The code from the original

---
+++ fbproc.php
@@ -0,0 +1,73 @@
+<?php
+
+use Respect\Validation\Validator as v;
+
+require_once "vendor/autoload.php";
+
+function outputError() {
+    echo json_encode(['success' => false, 'error' => 'Invalid URL']);
+    exit;
+}
+
+error_reporting(0);
+
+$url = $_POST['url'];
+
+// Validate URL
+$parse = parse_url($url);
+
+// Query may be missed, use default
+$parse['query'] = (!empty($parse['query'])) ? $parse['query'] : '';
+
+$check = v::arr()
+    ->key('scheme', v::startsWith('http'))
+    ->key('host',   v::domain()->oneOf(v::equals('facebook.com'), v::equals('www.facebook.com')))
+    ->key('path',   v::string())
+    ->key('query',  v::oneOf(v::startsWith('id='), v::string()))
+    ->validate($parse);
+
+if (!$check) {
+    outputError();
+}
+
+// If there's 'www' in the URL remove it
+$parse['host'] = str_replace('www.', '', $parse['host']);
+
+// Use graph API to get data
+$graphURL = 'http://graph.facebook.com';
+
+// Graph API URL depends on URL params
+if (!empty($parse['query'])) {
+    // If there's profile id, use it. It MUST be the only parameter
+    preg_match('/^id=(\d+)$/', $parse['query'], $matches);
+    
+    // Something gone bad?
+    if (empty($matches)) {
+        outputError();
+    }
+
+    // OK
+    $graphURL .= '/'.$matches[1];
+} else {
+    $graphURL .= $parse['path'];
+}
+
+// Collect information
+$graphJSON = file_get_contents($graphURL);
+
+// is page found?
+if (!$graphJSON) {
+    outputError();
+}
+
+// Parse json
+$graphData = json_decode($graphJSON);
+
+$res = [
+    'name'  => $graphData->name,
+    'id'    => $graphData->id,
+    'rss'   => "https://www.facebook.com/feeds/page.php?format=rss20&id={$graphData->id}",
+    'page'  => $url,
+];
+    
+echo json_encode(['success' => true, 'data' => $res]);
---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Facebook data</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+    
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+    
+</head>
+<body>
+    <div class="container">
+        <form class="form-main" id="facebook-form" data-toggle="validator" method="POST">
+            <h2 class="form-main-heading">Enter facebook URL to get data</h2>
+            <div class="form-group">
+                <input class="form-control" type="text" id="facebook-url" placeholder="Facebook URL" data-error="Please insert correct URL" pattern="^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})(\/[\d\w.?=]+)?$" required autofocus />
+                <span class="help-block with-errors"></span>
+            </div>
+            <button type="submit" class="btn btn-primary">Get info</button>
+        </form>
+        <br/>
+        <div class="alert alert-danger" id="error-message" role="alert"></div>
+
+        <div id="result">
+            <h3 class="form-main-heading">Facebook data</h3>
+            <label>Page ID:</label><span id="fb-id"></span><br/>
+            <label>Page name:</label><span id="fb-name"></span><br/>
+            <label>Rss Feed URL:</label><span id="fb-rss"></span><br/>
+            <label>Page URL:</label><span id="fb-page"></span><br/>
+        </div>
+    </div>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>    
+    <!-- Bootstrap validator -->
+    <script src="components/bootstrap-validator/js/validator.js"></script>
+    <!-- AJAX -->
+    <script>
+        $(document).ready(function() {
+            
+            // Hide error message
+            $('#error-message').hide();
+            $('#result').hide();
+            
+            // Process form submission
+            $("#facebook-form").validator().submit(function(event) {
+                var url = $("#facebook-url").val();
+                if (event.isDefaultPrevented()) {
+                    // Nothing to do
+                } else {
+                    $.ajax({
+                        type: 'post',
+                        dataType: 'json',
+                        data: {'url': url},
+                        url: 'fbproc.php',
+                        success: function(res) {
+                            if (res.success == true) {
+                                $('#result').show();
+                                $('#error-message').hide();
+                                
+                                // Set output text fields
+                                $('#fb-id').html("<mark>"+res.data['id']+"</mark>");
+                                $('#fb-name').html("<mark>"+res.data['name']+"</mark>");
+                                $('#fb-rss').html("<mark><a href='"+res.data['rss']+"'>"+res.data['rss']+"</a></mark>");
+                                $('#fb-page').html("<mark><a href='"+res.data['page']+"'>"+res.data['page']+"</a></mark>");
+                            } else {
+                                // Show error
+                                $('#result').hide();
+                                $('#error-message').html(res.error).show();
+                            }
+                        }
+                    });
+                    event.preventDefault();
+                }
+            });
+        
+            // Make sure there's protocol inserted in URL
+            $('#facebook-url').change(function() {
+                var url = $('#facebook-url').val();
+                $('#facebook-url').val(addProtocol(url)); 
+            });
+            
+        });
+        
+        // Add 'http://' to the url if it misses that
+        function addProtocol(url) {
+            if (url.search(/^http[s]?\:\/\//) == -1) {
+                url = 'http://' + url;
+            }            
+            return url;
+        }
+    </script>
+</body>
+</html>
+
+
